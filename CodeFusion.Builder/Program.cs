5fd08d00ad2f22039c49b6418b5f16d685a638ae
Christoph Koschel <christoph.koschel86@gmail.com>
2023-08-28 14:00:18 +0200

    Beauty fix

--- CodeFusion.Builder/Program.cs
+++ CodeFusion.Builder/Program.cs
@@ -6,42 +6,55 @@ using CodeFusion.VM;
 
 namespace CodeFusion.Builder;
 
-public class Program {
-    public static void Main(string[] args) {
+public class Program
+{
+    public static void Main(string[] args)
+    {
         string file = null;
         Platform platform = Platform.WINDOWS;
         string outputName = "a";
 
-        for (int i = 0; i < args.Length; i++) {
-            if (args[i] == "-p") {
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-p")
+            {
                 string value = args[++i];
-                if (value == "win") {
-                    platform  = Platform.WINDOWS;
-                } else if (value == "linux") {
+                if (value == "win")
+                {
+                    platform = Platform.WINDOWS;
+                }
+                else if (value == "linux")
+                {
                     platform = Platform.LINUX;
                 }
-            } else {
+            }
+            else
+            {
                 file = args[i];
             }
         }
 
-        if (file == null) {
-            Console.Error.WriteLine("No file provieded");
+        if (file == null)
+        {
+            Console.Error.WriteLine("No file provided");
             Environment.Exit(1);
         }
 
-        if (!outputName.EndsWith(".exe") && platform == Platform.WINDOWS) {
+        if (!outputName.EndsWith(".exe") && platform == Platform.WINDOWS)
+        {
             outputName += ".exe";
         }
 
         BinaryReader reader = new BinaryReader(new FileStream(file, FileMode.Open));
         Metadata metadata = Loader.ReadMainHeader(ref reader);
-        if ((metadata.flags & Metadata.EXECUTABLE) != Metadata.EXECUTABLE) {
+        if ((metadata.flags & Metadata.EXECUTABLE) != Metadata.EXECUTABLE)
+        {
             Console.Error.WriteLine("File must be executable");
             Environment.Exit(1);
         }
 
-        if ((metadata.flags & Metadata.CONTAINS_ERRORS) == Metadata.CONTAINS_ERRORS) {
+        if ((metadata.flags & Metadata.CONTAINS_ERRORS) == Metadata.CONTAINS_ERRORS)
+        {
             Console.Error.WriteLine("File contains errors");
             Environment.Exit(1);
         }
