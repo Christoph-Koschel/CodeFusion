1ae982a6a0257e833a4bcf3a4ce688352af77480
Christoph Koschel <christoph.koschel86@gmail.com>
2023-09-30 09:39:11 +0200

    Test program and updated test script

--- test.ps1
+++ test.ps1
@@ -1,18 +1,9 @@
 ﻿dotnet build
 
-Write-Host "Create lib.dll"
-dotnet run --project CodeFusion.ASM -- lib.cf int.cf -o lib.o.bin -t obj
-dotnet run --project CodeFusion.Dump -- lib.o.bin -h
-dotnet run --project CodeFusion.ASM -- lib.o.bin -o lib.bin -t lib
-dotnet run --project CodeFusion.Builder -- lib.bin -t lib
-
-Write-Host "Create a.exe"
-dotnet run --project CodeFusion.ASM -- test.cf int.cf -o test.o.bin -t obj -e entry
-dotnet run --project CodeFusion.Dump -- lib.o.bin -h
-dotnet run --project CodeFusion.ASM -- test.o.bin -o test.bin -e entry
-dotnet run --project CodeFusion.Builder -- test.bin
-
+Set-Location proj;
+Write-Host "Compile test.ils";
+dotnet run --project ../ISC/ISC.csproj -- test.ils
 Write-Host "Starting program";
-.\bin\a.exe
-Write-Host "Program finished";
-
+./bin/a.exe
+Write-Host "Finish program";
+Set-Location ..
